70d24ee26fef42790907989cb63085318bd27fec
Linda Nyika Mwende <linda.nyika@strathmore.edu>
2025-09-04 17:24:08 +0300

    Lesson 1

--- classes.php
+++
@@ -1,9 +0,0 @@
-<?php
-class structure  {
- public function heading (){
-    echo "<h1>Welcome to BBIT GRP E</h1>";
- }
-public function footer (){
-    echo "<footer>Copyright &copy; 2024 BBIT GRP E</footer>";
- }
-}
